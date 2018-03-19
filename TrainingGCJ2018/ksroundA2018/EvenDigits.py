9aff94b58a2ea45e7ef4f7f00e77aa93f6fa2aa9
chrislucas <christoffer.luccas@gmail.com>
2018-03-19 15:18:59 -0300

    solucao com complexidade alta. So resolve os casos de N pequeno

--- TrainingGCJ2018/ksroundA2018/EvenDigits.py
+++ TrainingGCJ2018/ksroundA2018/EvenDigits.py
@@ -2,5 +2,50 @@
 https://code.google.com/codejam/contest/9234486/dashboard#s=p0
 '''
 
+from sys import stdin as _in, stdout as _out
+
+
+def has_odd_number(n):
+    while n >= 1:
+        if (n & 1) == 1:
+            return True
+        n //= 10
+    return False
+
+
+_small = "../raw/out/a-small-practice.txt"
+_larger = "../raw/out/a-larger-practice.txt"
+
+
+# naive approach
+def solver():
+    _out = open(_larger, "w+")
+    cases = int(_in.readline())
+    for x in range(1, cases + 1):
+        n = int(_in.readline())
+        _copy_1 = n
+        _copy_2 = n
+        if (n & 1) == 1:
+            _copy_1 = n + 1
+            _copy_2 = n - 1
+
+        while has_odd_number(_copy_1) and has_odd_number(_copy_2):
+            _copy_1 += 2
+            _copy_2 -= 2
+
+        diff_1 = abs(n - _copy_1)
+        diff_2 = abs(n - _copy_2)
+        _answer = diff_1 if diff_1 < diff_2 else diff_2
+        _out.write("Case #%d: %d\n" % (x, _answer))
+
+
+def solver2():
+    _out = open(_larger, "w+")
+    cases = int(_in.readline())
+    for x in range(1, cases + 1):
+        pass
+
+solver()
+
 if __name__ == '__main__':
     pass
